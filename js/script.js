9fce7c2834d3642d38c996c43de217bf00e6ac11
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2023-03-27 21:52:46 -0300

    finish project

---
+++ js/script.js
@@ -0,0 +1,32 @@
+import { Modal } from './modal.js';
+import { AlertError } from "./alert-error.js";
+import { IMC, notANumber } from "./utils.js"
+
+const form = document.querySelector('form');
+const inputWeight = document.querySelector('#weight');
+const inputHeight = document.querySelector('#height');
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+  const weight =inputWeight.value;
+  const height = inputHeight.value;
+
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if(showAlertError) {
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
+
+  const result = IMC(weight, height);
+  const message = `Seu IMC é de ${result}`;
+
+  Modal.message.innerText = message;
+  Modal.open()
+}
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
